8f72a257e4bdbb11e1e3ee8abedb31e0f0ce5eb0
bright <bright@brights-MacBook-Air.local>
2023-10-05 21:46:52 -0700

    add matmul() and @ operator

--- studio/matmul.py
+++ studio/matmul.py
@@ -11,6 +11,14 @@ end_time = time.time()
 spent_time = (end_time - start_time)
 print(f"matmul spent time: {spent_time}, putput: {output}")
 
+#use @ operator: @ operator is faster than matmul
+start_time = time.time()
+output = tensor @ tensor
+end_time = time.time()
+spent_time = (end_time - start_time)
+print(f"operator @ spent time: {spent_time}, putput: {output}")
+
+
 #do the calculation manually
 start_time = time.time()
 prod = 0;
@@ -20,4 +28,15 @@ end_time = time.time()
 print(f"calculate prod manually. spent time: {end_time-start_time}, prod: {prod}")
 
 ## conclusion: use matmul is much faster than manually compute prod when data is larger
+
+## matmul dimensions
+t1 = torch.rand(2,3)
+t2 = torch.rand(3,2)
+t3 = t1 @ t2
+print(f"t1 shape: {t1.shape}; t2 shape: {t2.shape}; t1@t2 shape: {t3.shape}")
+
+t1 = torch.rand(4,6)
+t2 = torch.transpose(t1, 0, 1)
+t3 = t1 @ t2
+print(f"t1 shape: {t1.shape}; t2 shape: {t2.shape}; t1@t2 shape: {t3.shape}")
 
--- studio/readme.md
+++ studio/readme.md
@@ -1,3 +1,11 @@
+- ref:
+  - https://www.youtube.com/watch?v=V_xro1bcAuA
+  - learn pytorch:  https://www.learnpytorch.io/
+  -  Zero to Mastery course github:  https://github.com/mrdbourke/pytorch-deep-learning
+  - colab:  https://colab.research.google.com/notebooks/welcome.ipynb
+  - pytorch github: https://github.com/pytorch/pytorch
+
+
 - I created pytorch conda environment to run this project. Do following to enable it from VS Code:
   - opne Command Palette (⇧⌘P)
   - search "python: Select", then select "Python: select interpreter" and choose the environment
