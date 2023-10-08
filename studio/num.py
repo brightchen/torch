1092c4e62f76913ecd1e9800dd1d93b1033020ee
bright <bright@brights-MacBook-Air.local>
2023-10-07 22:57:47 -0700

    torch integrate with numpy

---
+++ studio/num.py
@@ -0,0 +1,15 @@
+
+import torch
+#NOTES: should not name this file as numpy.py, otherwise "import numpy" actually import from this file
+#And cause circular import issue
+import numpy as np
+
+#torch.from_numpy
+#torch.Tensor.numpy
+
+array = np.arange(1.0, 8.0)
+
+
+
+
+
