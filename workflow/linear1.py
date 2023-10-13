3956c06d6432828caa171c4f068e55bcb1733310
bright <bright@brights-MacBook-Air.local>
2023-10-12 22:31:31 -0700

    add model train/test for workflow

--- workflow/linear1.py
+++ workflow/linear1.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import matplotlib.pyplot as plt
 
 weight = 0.7
@@ -29,11 +30,15 @@ def plot_predictions(train_data=X_train,
                      train_labels=y_train, 
                      test_data=X_test, 
                      test_labels=y_test, 
-                     predictions=None):
+                     predictions=None,
+                     show=False):
   """
   Plots training data, test data and compares predictions.
   """
-  plt.figure(figsize=(10, 7))
+
+  cfg = plt.figure(figsize=(10, 7))
+  #set blackground color, seems not work
+  cfg.set_facecolor('lightgray')  #'black'
 
   # Plot training data in blue
   plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
@@ -49,6 +54,115 @@ def plot_predictions(train_data=X_train,
   plt.legend(prop={"size": 14});
 
   # need this to show the graph when running in console
-  plt.show()
+  if show:
+     plt.show()
 
 plot_predictions();
+
+
+# Create a Linear Regression model class
+class LinearRegressionModel(nn.Module): # <- almost everything in PyTorch is a nn.Module (think of this as neural network lego blocks)
+    def __init__(self):
+        super().__init__() 
+        weightsParam = torch.randn(1, # <- start with random weights/bias (this will get adjusted as the model learns)
+                                   dtype=torch.float) # <- PyTorch loves float32 by default
+        self.weights = nn.Parameter(weightsParam,
+                                    requires_grad=True)  # <- can we update this value with gradient descent?)
+        
+        # biasParam should use the different instance from weightsParam, otherwise value not correct.
+        biasParam = torch.randn(1, # <- start with random weights/bias (this will get adjusted as the model learns)
+                                dtype=torch.float) # <- PyTorch loves float32 by default
+        self.bias = nn.Parameter(biasParam, # <- PyTorch loves float32 by default
+                                 requires_grad=True) # <- can we update this value with gradient descent?))
+
+    # Forward defines the computation in the model
+    def forward(self, x: torch.Tensor) -> torch.Tensor: # <- "x" is the input data (e.g. training/testing features)
+        return self.weights * x + self.bias # <- this is the linear regression formula (y = m*x + b)
+    
+
+# Set manual seed since nn.Parameter are randomly initialzied
+torch.manual_seed(42)
+
+# Create an instance of the model (this is a subclass of nn.Module that contains nn.Parameter(s))
+model_0 = LinearRegressionModel()
+
+# Check the nn.Parameter(s) within the nn.Module subclass we created
+list(model_0.parameters())
+print("== parameters: \n", list(model_0.parameters()))
+print("== state: \n", model_0.state_dict())
+
+# this should be test the model. When train the model???
+# Make predictions with model
+with torch.inference_mode(): 
+    y_preds = model_0(X_test)
+
+# Note: in older PyTorch code you might also see torch.no_grad()
+# with torch.no_grad():
+#   y_preds = model_0(X_test)
+
+# Check the predictions
+print(f"========== Check the predictions ==========")
+print(f"Number of testing samples: {len(X_test)}") 
+print(f"Number of predictions made: {len(y_preds)}")
+print(f"Predicted values:\n{y_preds}")
+
+#visualize the difference
+plot_predictions(predictions=y_preds, show=False)
+
+loss_fn = nn.L1Loss() # MAE loss is same as L1Loss
+
+# Create the optimizer
+optimizer = torch.optim.SGD(params=model_0.parameters(), # parameters of target model to optimize
+                            lr=0.01) # learning rate (how much the optimizer should change parameters at each step, higher=more (less stable), lower=less (might take a long time))
+
+
+############ train and test loop ############
+torch.manual_seed(42)
+
+# Set the number of epochs (how many times the model will pass over the training data)
+epochs = 100
+
+# Create empty loss lists to track values
+train_loss_values = []
+test_loss_values = []
+epoch_count = []
+
+for epoch in range(epochs):
+    #################### Training
+
+    # Put model in training mode (this is the default state of a model)
+    model_0.train()
+
+    # 1. Forward pass on train data using the forward() method inside 
+    y_pred = model_0(X_train)
+    # print(y_pred)
+
+    # 2. Calculate the loss (how different are our models predictions to the ground truth)
+    loss = loss_fn(y_pred, y_train)
+
+    # 3. Zero grad of the optimizer
+    optimizer.zero_grad()
+
+    # 4. Loss backwards
+    loss.backward()
+
+    # 5. Progress the optimizer
+    optimizer.step()
+
+    ############## Testing
+    # Put the model in evaluation mode
+    model_0.eval()
+
+    with torch.inference_mode():
+      # 1. Forward pass on test data
+      test_pred = model_0(X_test)
+
+      # 2. Caculate loss on test data
+      test_loss = loss_fn(test_pred, y_test.type(torch.float)) # predictions come in torch.float datatype, so comparisons need to be done with tensors of the same type
+
+      # Print out what's happening
+      if epoch % 10 == 0:
+        epoch_count.append(epoch)
+        train_loss_values.append(loss.detach().numpy())
+        test_loss_values.append(test_loss.detach().numpy())
+        print(f"Epoch: {epoch} | MAE Train Loss: {loss} | MAE Test Loss: {test_loss} ")
